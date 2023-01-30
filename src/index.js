a1a687b7eea29ab22728f82ec4fd8d3e9e72b546
Kwon Dae-ho <101388919+vaaast-lake@users.noreply.github.com>
2023-01-30 14:11:18 +0900

    router & query

--- src/index.js
+++ src/index.js
@@ -3,11 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import VideoList from './components/VideoList/VideoList';
+import NotFound from './components/NotFound/NotFound';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import WatchVideo from './components/WatchVideo/WatchVideo';
+
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {index: true, element: <VideoList />},
+      {path: '/videos/:searchId', element: <VideoList />},
+      {path: '/videos/watch/:embedId', element: <WatchVideo />}
+    ]
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
